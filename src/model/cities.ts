a4e51a2eb705f9e272fcfd87de640450d9afbf32
Wong Jia Hau <wong.jh@mydidian.com>
2020-02-23 00:35:21 +0800

    Coded map for blue and black cities

--- src/model/cities.ts
+++ src/model/cities.ts
@@ -1,10 +1,24 @@
 export type City = {
   color: 'red' | 'blue' | 'yellow' | 'black'
   name: CityName
-  connectedTo: CityName[]
+  connectedTo: {
+    name: CityName
+    cannotBuildRailRoad?: boolean
+  }[]
+  position: {
+    /**
+     * Zero-based.
+     */
+    column: number
+
+    /**
+     * One-based.
+     */
+    row: number
+  }
   isPort?: boolean
 }
-export type CityName 
+export type CityName
   = 'A Coruna'
   | 'Santiago De Compostela'
   | 'Vigo'
@@ -50,11 +64,137 @@ export type CityName
   | 'Barcelona'
   | 'Huesca'
   | 'Palma De Mallorca'
+  | 'Cartagena'
 
 
-export const cities: City[] = [
-  {color: 'blue', name: 'A Coruna', connectedTo: ['Santiago De Compostela', 'Gijon'], isPort: true},
-  {color: 'blue', name: 'Santiago De Compostela', connectedTo: ['A Coruna', 'Vigo', 'Ourense']},
-  {color: 'blue', name: 'Vigo', connectedTo: ['Santiago De Compostela', 'Porto', 'Ourense', 'Braga'], isPort: true},
-  {color: 'blue', name: 'Ourense', connectedTo: ['Santiago De Compostela', 'Vigo', 'Leon']},
-]
+export const cities: City[] = [{
+  color: 'blue',
+  name: 'A Coruna',
+  connectedTo: [{ name: 'Santiago De Compostela' }, { name: 'Gijon' }],
+  isPort: true,
+  position: { column: 2, row: 1 }
+}, {
+  color: 'blue',
+  name: 'Santiago De Compostela',
+  connectedTo: [{ name: 'A Coruna' }, { name: 'Vigo' }, { name: 'Ourense' }],
+  position: { column: 2, row: 2 }
+}, {
+  color: 'blue',
+  name: 'Vigo',
+  connectedTo: [{ name: 'Santiago De Compostela' }, { name: 'Porto' }, { name: 'Ourense' }, { name: 'Braga' }],
+  isPort: true,
+  position: { column: 1, row: 4 }
+}, {
+  color: 'blue',
+  name: 'Ourense',
+  connectedTo: [{ name: 'Santiago De Compostela' }, { name: 'Vigo' }, { name: 'Leon' }],
+  position: { column: 5, row: 4 }
+}, {
+  color: 'blue',
+  name: 'Braga',
+  connectedTo: [{ name: 'Vigo' }, { name: 'Porto' }, { name: 'Salamanca' }],
+  position: { column: 4, row: 6 }
+}, {
+  color: 'blue',
+  name: 'Porto',
+  isPort: true,
+  connectedTo: [{ name: 'Braga' }, { name: 'Vigo' }, { name: 'Coimbra' }, { name: 'Lisboa' }],
+  position: { column: 1, row: 7 }
+}, {
+  color: 'blue',
+  name: 'Salamanca',
+  connectedTo: [{ name: 'Leon' }, { name: 'Valladolid' }, { name: 'Madrid' }, { name: 'Caceres' }, { name: 'Braga' }],
+  position: { column: 7, row: 7 }
+}, {
+  color: 'blue',
+  name: 'Coimbra',
+  connectedTo: [{ name: 'Caceres' }, { name: 'Lisboa' }, { name: 'Porto' }],
+  position: { column: 3, row: 9 }
+}, {
+  color: 'blue',
+  name: 'Caceres',
+  connectedTo: [{ name: 'Salamanca' }, { name: 'Toledo' }, { name: 'Badajoz' }, { name: 'Coimbra' }],
+  position: { column: 6, row: 11 }
+}, {
+  color: 'blue',
+  name: 'Lisboa',
+  isPort: true,
+  connectedTo: [{ name: 'Porto' }, { name: 'Coimbra' }, { name: 'Evora' }, { name: 'Albufeira' }],
+  position: { column: 0, row: 13 }
+}, {
+  color: 'blue',
+  name: 'Evora',
+  connectedTo: [{ name: 'Badajoz' }, { name: 'Huelva' }, { name: 'Lisboa' }],
+  position: { column: 3, row: 14 }
+}, {
+  color: 'blue',
+  name: 'Albufeira',
+  isPort: true,
+  connectedTo: [{ name: 'Huelva' }, { name: 'Lisboa' }],
+  position: { column: 2, row: 17 }
+}, {
+  color: 'black',
+  name: 'Toledo',
+  connectedTo: [{ name: 'Madrid' }, { name: 'Ciudad Real' }, { name: 'Caceres' }],
+  position: { column: 9, row: 10 }
+}, {
+  color: 'black',
+  name: 'Badajoz',
+  connectedTo: [{ name: 'Caceres' }, { name: 'Ciudad Real' }, { name: 'Cordoba' }, { name: 'Evora' }],
+  position: { column: 5, row: 13 }
+}, {
+  color: 'black',
+  name: 'Ciudad Real',
+  connectedTo: [{ name: 'Madrid' }, { name: 'Albacete' }, { name: 'Cordoba' }, { name: 'Badajoz' }, { name: 'Toledo' }],
+  position: { column: 11, row: 12 },
+}, {
+  color: 'black',
+  name: 'Cordoba',
+  connectedTo: [{ name: 'Ciudad Real' }, { name: 'Jaen' }, { name: 'Malaga' }, { name: 'Sevilla' }, { name: 'Badajoz' }],
+  position: { column: 9, row: 15 }
+}, {
+  color: 'black',
+  name: 'Jaen',
+  connectedTo: [{ name: 'Albacete' }, { name: 'Granada' }, { name: 'Cordoba' }],
+  position: { column: 11, row: 15 }
+}, {
+  color: 'black',
+  name: 'Huelva',
+  isPort: true,
+  connectedTo: [{ name: 'Sevilla' }, { name: 'Cadiz', cannotBuildRailRoad: true }, { name: 'Albufeira' }, { name: 'Evora' }],
+  position: { column: 5, row: 17 }
+}, {
+  color: 'black',
+  name: 'Sevilla',
+  connectedTo: [{ name: 'Cordoba' }, { name: 'Malaga' }, { name: 'Cadiz' }, { name: 'Huelva' }],
+  position: { column: 7, row: 17 }
+}, {
+  color: 'black',
+  name: 'Granada',
+  connectedTo: [{ name: 'Jaen' }, { name: 'Almeria' }, { name: 'Malaga' }],
+  position: { column: 12, row: 16 }
+}, {
+  color: 'black',
+  name: 'Cadiz',
+  isPort: true,
+  connectedTo: [{ name: 'Sevilla' }, { name: 'Gibraltar', cannotBuildRailRoad: true }, { name: 'Huelva', cannotBuildRailRoad: true }],
+  position: {column: 6, row: 19}
+}, {
+  color: 'black',
+  name: 'Malaga',
+  isPort: true,
+  connectedTo: [{name: 'Granada' }, {name: 'Almeria'}, {name: 'Gibraltar', cannotBuildRailRoad: true}, {name: 'Sevilla'}, {name: 'Cordoba'}],
+  position: {column: 10, row: 18}
+}, {
+  color: 'black',
+  name: 'Almeria',
+  isPort: true,
+  connectedTo: [{name: 'Cartagena'}, {name: 'Malaga'}, {name: 'Granada'}],
+  position: {column: 14, row: 18}
+}, {
+  color: 'black',
+  name: 'Gibraltar',
+  isPort: true,
+  connectedTo: [{name: 'Malaga', cannotBuildRailRoad: true}, {name: 'Cadiz', cannotBuildRailRoad: true},],
+  position: {column: 8, row: 19}
+}]
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,12 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { setupGame } from '../model/setup-game';
+import { ArcherContainer, ArcherElement } from 'react-archer';
+import { CityName } from '../model/cities';
+
 
 function App() {
+  const [state, setState] = React.useState(setupGame())
+  const getCityId = (cityName: CityName) => cityName.replace(/\s/gi, '')
+  
   return (
-    <div style={{display: 'grid', height: '100vh', width: '100vw'}}>
-
-    </div>
+    <ArcherContainer strokeColor='grey'>
+      <div style={{
+        display: 'grid', 
+        height: '100vh', 
+        width: '100vw',
+        gridTemplateColumns: 'repeat(24, 1fr)',
+        gridTemplateRows: 'repeat(19, 1fr)',
+        gridGap: '18px'
+      }}>
+        {state.cities.map((city, index) => (
+          <ArcherElement
+            key={index}
+            id={getCityId(city.name)}
+            style={{
+              gridColumn: city.position.column + 1, 
+              gridRow: city.position.row,
+            }}
+            relations={city.connectedTo.map(city => ({
+              targetId: getCityId(city.name),
+              targetAnchor: 'middle',
+              sourceAnchor: 'middle',
+              ...(city.cannotBuildRailRoad ? {style: {strokeDasharray: '8'}} : {})
+            }))}
+          >
+            <div key={index} style={{
+              backgroundColor: city.color,
+              borderRadius: '24px',
+              width: '24px',
+              height: '24px',
+              fontSize: '8px',
+              color: 'white'
+            }}>
+              {city.name}
+            </div>
+          </ArcherElement>
+        ))}
+      </div>
+    </ArcherContainer>
   );
 }
 
